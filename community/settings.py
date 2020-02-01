73904a3eae5b3fd05a37e25a7b977ee4866c2d58
crazy-coding <dennischo122@gmail.com>
2020-02-02 05:07:23 +0800

    for deploy

--- community/settings.py
+++ community/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/community/static'
 
-# MAIN_SITE_URL = 'http://unstable-v.raplev.com'
-MAIN_SITE_URL = 'http://localhost:8000'
+MAIN_SITE_URL = 'http://unstable-v.raplev.com'
